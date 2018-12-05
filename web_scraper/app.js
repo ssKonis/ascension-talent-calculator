40e474559124da9365f1a08475f5e8b4605e0b77
James Wilkinson <jdwilkinson1994@gmail.com>
2018-12-05 15:04:47 +0800

    Webscraper Hello World

--- web_scraper/app.js
+++ web_scraper/app.js
@@ -1,4 +1,27 @@
-const rp = require('request-promise');
-const url = 'https://project-ascension.com/development/builds#/talentsandabilities/editor/'
+const curl = require("curl");
+const jsdom = require("jsdom");
+const url = "http://127.0.0.1:5500/web_scraper/Ascension%20Classless%20Realm.html#/talentsandabilities/editor/";
 
-rp(url)
+
+
+function parseData(html) {
+  const { JSDOM } = jsdom;
+  const dom = new JSDOM(html);
+  const $ = (require('jquery'))(dom.window);
+  let main = $('#root').html();
+  console.log(main);
+
+
+
+
+}
+
+curl.get(url, null, (err, resp, body) => {
+  if (resp.statusCode == 200) {
+    parseData(body);
+  }
+  else {
+    //some error handling
+    console.log("error while fetching url");
+  }
+});
