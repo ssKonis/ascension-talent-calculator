b7321c59b56fa31d11cc720bd96f190aa879a6c8
James Wilkinson <jdwilkinson1994@gmail.com>
2018-12-05 16:26:15 +0800

    found id, location and image

--- web_scraper/app.js
+++ web_scraper/app.js
@@ -8,12 +8,30 @@ function parseData(html) {
   const { JSDOM } = jsdom;
   const dom = new JSDOM(html);
   const $ = (require('jquery'))(dom.window);
-  let main = $('#root').html();
-  console.log(main);
 
+  /*Select first table for druids*/
+  let table = $('.atc-editor-classtabbarcontent-content-wrapper-talenttree-table > tbody').first().children();
 
+  /* For each row*/
+  for (let row = 0; row < table.length; row++) {
+    let table_row = $(table[row]).children();
+    /*For Each column, skip first column*/
+    for (let col = 1; col < table_row.length; col++) {
+      let table_col = $(table_row[col]).children();
 
+      let iconExists = table_col.length; /* Length > than 1 if there is an icon */
+      if (iconExists) {
+        let image = $(table_col).find('img').attr('src')
+        let id = $(table_col).find('.atc-editor-classtabbarcontent-content-wrapper-talenttree-table-talent-container-content-wrapper-talent-images-overlay-gray').attr('data-ascension-tooltip-id');
+        console.log("id = ", id, "image = ", image);
+      }
+      else {
+        console.log("Empty")
 
+      }
+
+    }
+  }
 }
 
 curl.get(url, null, (err, resp, body) => {
