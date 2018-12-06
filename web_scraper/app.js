381d52ca50fc21b47b34f975b6f9ae95e50c8493
James Wilkinson <jdwilkinson1994@gmail.com>
2018-12-06 14:40:29 +0800

    Created Map json file, but Ids for talent ranks are incorrect

--- web_scraper/app.js
+++ web_scraper/app.js
@@ -1,19 +1,21 @@
 const curl = require("curl");
 const jsdom = require("jsdom");
-const url = "http://127.0.0.1:5500/web_scraper/Ascension%20Classless%20Realm.html#/talentsandabilities/editor/";
+const url = "http://127.0.0.1:5500/Ascension%20Classless%20Realm.html#/talentsandabilities/editor/";
 
-var Talent = function (row, col, image) {
-  this.row = row;
-  this.col = col;
-  this.image = image;
-  this.tiers = {
+var Talent = function (row, col, max_rank) {
+  this.location = {
+    row: row,
+    col: col
+  };
+  this.data = [{
+    /*Rank*/
+    /*Image*/
     /*id*/
-    /*rank*/
-    /*tool tip?*/
-  }
+  }]
+  this.max_rank = max_rank;
 }
 
-
+var map = [];
 function parseData(html) {
   const { JSDOM } = jsdom;
   const dom = new JSDOM(html);
@@ -21,6 +23,7 @@ function parseData(html) {
 
   /*Select first table for druids*/
   let table = $('.atc-editor-classtabbarcontent-content-wrapper-talenttree-table > tbody').first().children();
+
 
   /* For each row*/
   for (let row = 0; row < table.length; row++) {
@@ -36,23 +39,50 @@ function parseData(html) {
         let rank_range = $(table_col).find('.atc-editor-classtabbarcontent-content-wrapper-talenttree-table-talent-container-content-wrapper-talent-text-gray').html();
         let cur_rank = rank_range.charAt(0);
         let max_rank = rank_range.charAt(2);
-        console.log("id = ", id, "image = ", image, "rank = ", rank_range, "current_rank =", cur_rank, "max_rank = ", max_rank);
+        let talent = new Talent((row + 1), col, max_rank);
+
+        for (let k = 0; k < max_rank; k++) {
+          let data = {}
+          data.id = parseInt(id) + k;
+          data.rank = parseInt(cur_rank) + k;
+          data.image = image.split("/")[2];
+          talent.data.push(data);
+        }
+
+        map.push(talent);
+
       }
       else {
-        console.log("Empty")
+        let talent = new Talent((row + 1), col, 1)
+        map.push(talent);
 
       }
 
     }
   }
 }
+const fs = require('fs')
 
 curl.get(url, null, (err, resp, body) => {
   if (resp.statusCode == 200) {
     parseData(body);
+
+    map.forEach(item => {
+      console.log(JSON.stringify(item.data, null, 2));
+
+    });
+
+    fs.writeFile("./text.json", JSON.stringify(map), (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      };
+      console.log("File has been created");
+    })
   }
   else {
     //some error handling
     console.log("error while fetching url");
   }
 });
+
