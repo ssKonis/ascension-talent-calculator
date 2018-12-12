05e6650d9f427babc24c7621fea5d33bd60985c0
James Wilkinson <jdwilkinson1994@gmail.com>
2018-12-12 16:49:11 +0800

    Got all ids for each rank for each class. just need to remove duplicates

--- web_scraper/app.js
+++ web_scraper/app.js
@@ -1,13 +1,10 @@
 var fs = require('fs');
 const jsdom = require("jsdom");
-var events = require('events');
 
 const cheerio = require('cheerio');
 const curl = require('curl');
 
 var items = [];
-
-
 
 function Talent(max_rank) {
   this.data = [
@@ -38,7 +35,7 @@ function getRootID(html, class_name) {
       let row_length = table_row.length
       if ((class_name == 'paladin.html') && (i == 1)) {
         row_length = 5;
-        console.log("Found paladin prot")
+        // console.log("Found paladin prot")
       }
       for (let col = 1; col < row_length; col++) {
         let table_col = $(table_row[col]).children();
@@ -51,8 +48,9 @@ function getRootID(html, class_name) {
           let cur_rank = rank_range.charAt(0);
           let max_rank = parseInt(rank_range.charAt(2));
           let talent = new Talent(max_rank);
+          talent.class_name = class_name.split('.')[0]
 
-          for (let k = 0; k < max_rank; k++) {
+          for (let k = 0; k < 1; k++) {
             let data = {}
             data.id = parseInt(id) + k; /*This method does not always return correct ID*/
             data.rank = parseInt(cur_rank) + k + 1;
@@ -78,24 +76,26 @@ function getRootID(html, class_name) {
 }
 
 
-// var pages = ['druid.html', 'hunter.html', 'mage.html', 'paladin.html', 'priest.html', 'rogue.html', 'shaman.html', 'warlock.html', 'warrior.html'];
-// var count = 0;
-// pages.forEach(html => {
-//   getRootID(html, pages[count]);
-//   count += 1;
-// });
+var pages = ['druid.html', 'hunter.html', 'mage.html', 'paladin.html', 'priest.html', 'rogue.html', 'shaman.html', 'warlock.html', 'warrior.html'];
+var count = 0;
+pages.forEach(html => {
+  getRootID(html, pages[count]);
+  count += 1;
+});
 
-getRootID('warrior.html', 'warrior.html')
+// getRootID('warrior.html', 'warrior.html')
 // map.forEach(item => {
 //   console.log(JSON.stringify(item.data, null, 2));
 // });
 
 
-function getNextID(html, page_num) {
+function getNextID(html, class_id) {
   const { JSDOM } = jsdom;
   const dom = new JSDOM(html);
   const $ = (require('jquery'))(dom.window);
-  let table = $('tbody').first().html()
+  let table = $('tbody').first().html();
+
+  let class_name = pages[class_id].split('.')[0]
   $(table).each(function (row) {
     if (row % 2 == 0) { /*For some reason an extra row is being pulled, only selects actual ids*/
       let id = parseInt($(this).children().eq(0).text())
@@ -107,7 +107,8 @@ function getNextID(html, page_num) {
       let item = {
         name: name,
         id: id,
-        rank: rank
+        rank: rank,
+        class_name: class_name
       }
       items.push(item);
     }
@@ -128,11 +129,11 @@ function getNumPages(html) {
 
 }
 
-function req(url, page_num) {
+function req(url, page_num, class_id) {
   curl.get(url, null, (err, resp, body) => {
     if (resp.statusCode == 200) {
       console.log('Page: ', page_num)
-      getNextID(body, page_num);
+      getNextID(body, class_id);
       // console.log("==================================================")
     }
     else {
@@ -144,39 +145,64 @@ function req(url, page_num) {
 
 
 
-let home = 'https://data.project-ascension.com/spells?class=8&page=1&type=Talent';
+for (let j = 1; j < 9; j++) {
+  let home = 'https://data.project-ascension.com/spells?class=' + j + '&page=1&type=Talent';
 
-curl.get(home, null, (err, resp, body) => {
-  if (resp.statusCode == 200) {
-    let n_pages = getNumPages(body);
-    for (let i = 0; i < n_pages; i++) {
-      let url = 'https://data.project-ascension.com/spells?class=8&page=' + i + '&type=Talent';
-      req(url, i + 1);
+  curl.get(home, null, (err, resp, body) => {
+    if (resp.statusCode == 200) {
+      let n_pages = getNumPages(body);
+      for (let i = 0; i < n_pages; i++) {
+        let url = 'https://data.project-ascension.com/spells?class=' + j + '&page=' + i + '&type=Talent';
+        req(url, i + 1, j);
 
+      }
+    }
+    else {
+      //some error handling
+      console.log("error while fetching url");
+    }
+
+  });
+}
+
+
+
+
+
+
+
+
+var superMap = map.slice()
+setTimeout(function () {
+
+  for (let j = 0; j < map.length; j++) {
+    if (map[j].data[0] != undefined) {
+      for (let i = 0; i < items.length; i++) {
+        if (map[j].data[0].id == items[i].id) {
+          superMap[j].name = items[i].name;
+          for (let k = 0; k < items.length; k++) {
+            if ((superMap[j].name == items[k].name) && (items[k].rank != 1) && (map[j].class_name == items[k].class_name)) {
+              let data = {}
+              data.id = items[k].id;
+              data.rank = items[k].rank;
+              superMap[j].data.push(data);
+            }
+          }
+        }
+      }
     }
   }
-  else {
-    //some error handling
-    console.log("error while fetching url");
-  }
-
-});
-
-
-// fs.writeFile("./test.json", JSON.stringify(items), (err) => {
-//   if (err) {
-//     console.error(err);
-//     return;
-//   };
-//   console.log("File has been created");
-// })
-
-setTimeout(function () {
-  items.forEach(item => {
+  superMap.forEach(item => {
     console.log(JSON.stringify(item, null, 2));
-  });
-  map.forEach(item => {
-    console.log(JSON.stringify(item, null, 2));
-  });
-}, 10000)
+
+  })
+
+  // fs.writeFile("./test.json", JSON.stringify(superMap), (err) => {
+  //   if (err) {
+  //     console.error(err);
+  //     return;
+  //   };
+  //   console.log("File has been created");
+  // })
+}, 30000)
 
